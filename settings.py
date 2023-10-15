ab45ee97496a5a08240a6bbe49f7153644f39f94
RacerAngler <sinredhot.pokemongo@gmail.com>
2023-10-15 09:40:13 +0900

    Two BIOME table option

--- settings.py
+++ settings.py
@@ -1,6 +1,15 @@
 # Biome rectangle
-# Aogaki
-rect = [[35.29452, 134.96887], [35.24589, 135.04371]]
+# NY city
+rect = [[40.8733, -74.0331], [40.63112, -73.8288]]
 
 # ISO 8601 format datetime
-dt = '2023-10-12T09:00:00+09:00'
+dt = '2023-10-16T00:04:00-04:00'
+
+# BIOME Tables
+# If habitat changes daily in the order Forest -> Desert -> Swamp ->,
+# then use table below.
+BIOME = ['Swamp', 'Desert', 'Forest']
+
+# If habitat changes daily in the order Forest -> Swamp -> Desert ->,
+# then use table below. * remove '#' charactor
+#BIOME = ['Forest', 'Desert', 'Swamp']
